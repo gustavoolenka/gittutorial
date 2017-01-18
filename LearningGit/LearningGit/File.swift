3f98e43af8bec109a24e19e600896ef287f9cdfa
lucasferraco <lucasferracof@gmail.com>
2017-01-18 14:54:28 -0200

    Primeiro Commit

---
+++ LearningGit/LearningGit/File.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  LearningGit
+//
+//  Created by Lucas Ferraço on 18/01/17.
+//  Copyright © 2017 Lucas Ferraço. All rights reserved.
+//
+
+import Foundation
+
+class Olenka {
+    var primeiroNome: String!
+    var tamanhoP: Int!
+    
+}
---
+++ LearningGit/LearningGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearningGit
+//
+//  Created by Lucas Ferraço on 18/01/17.
+//  Copyright © 2017 Lucas Ferraço. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
